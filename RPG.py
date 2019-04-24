57a516f949a6be92a569062995bcf8e82b855fe7
Chris-Humphrey <clhumphrey21@gmail.com>
2019-04-24 15:08:59 -0500

    Added unbeatable zombies

--- RPG.py
+++ RPG.py
@@ -32,14 +32,19 @@ class Goblin(Character):
     def __init__(self, name, health, power):
         super().__init__(name, health, power)
 
+class Zombie(Character):
+    def __init__(self, name, health, power):
+        super().__init__(name, health, power)
+
 
 def main():
     hero = Hero("Hero", 10, 5)
     enemy = Goblin("Goblin", 6, 2)
+    zombie = Zombie("Zombie", 1000, 2)
 
-    while enemy.alive() and hero.alive():
+    while zombie.alive() and hero.alive():
         hero.print_status()
-        enemy.print_status()
+        zombie.print_status()
         print()
         print("What do you want to do?")
         print("1. fight goblin")
@@ -48,11 +53,16 @@ def main():
         print("> ", end=' ')
         raw_input = input()
         if raw_input == "1":
+            # # Hero attacks goblin
+            # hero.attack(enemy)
+            # print("You do {} damage to the goblin.".format(hero.power))
+            # if enemy.health <= 0:
+            #     print("The goblin is dead.")
             # Hero attacks goblin
-            hero.attack(enemy)
-            print("You do {} damage to the goblin.".format(hero.power))
+            hero.attack(zombie)
+            print("You do {} damage to the {}.".format(hero.power, zombie.name))
             if enemy.health <= 0:
-                print("The goblin is dead.")
+                print(f"The {zombie.name} is dead.")
         elif raw_input == "2":
             pass
         elif raw_input == "3":
@@ -61,10 +71,16 @@ def main():
         else:
             print("Invalid input {}".format(raw_input))
 
-        if enemy.health > 0:
-            # Goblin attacks hero
-            enemy.attack(hero)
-            print("The goblin does {} damage to you.".format(enemy.power))
+        # if enemy.health > 0:
+        #     # Goblin attacks hero
+        #     enemy.attack(hero)
+        #     print("The goblin does {} damage to you.".format(enemy.power))
+        #     if hero.health <= 0:
+        #         print("You are dead.")
+        if zombie.health > 0:
+            # Zombie attacks hero
+            zombie.attack(hero)
+            print("The {} does {} damage to you.".format(zombie.name, zombie.power))
             if hero.health <= 0:
                 print("You are dead.")
 
